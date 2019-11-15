4496e2b1e67576e08228a0535ed1c5d474eea73f
Derik Oliveira <derikoliveira03@gmail.com>
2019-11-15 13:19:47 -0300

    Adding and styling local image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
+
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const GameOverScreen = props => {
   const {roundsNumber, userNumber, onRestartGame} = props;
   return (
     <View style={styles.screen}>
-      <Text>The Game is Over!</Text>
-      <Text>Number of rounds: {roundsNumber}</Text>
-      <Text>Number was: {userNumber}</Text>
+      <TitleText>The Game is Over!</TitleText>
+      <View style={styles.imageContainer}>
+        <Image source={require('../assets/images/success.png')}
+               style={styles.image} resizeMode='cover'/>
+      </View>
+      <BodyText>Number of rounds: {roundsNumber}</BodyText>
+      <BodyText>Number was: {userNumber}</BodyText>
       <Button title='New Game' onPress={onRestartGame}/>
     </View>
   );
@@ -18,6 +25,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: 'black',
+    overflow: 'hidden', // it 'cuts' anything that surpasses the size of the container
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   }
 });
 
